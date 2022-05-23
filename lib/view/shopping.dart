b0a2846bfd9adb136ca8c59bcb516c6384954eb9
Emilio kariuki <emilio113kariuki@gmail.com>
2022-05-23 11:17:02 +0300

    removed the list view

--- lib/view/shopping.dart
+++ lib/view/shopping.dart
@@ -15,34 +15,30 @@ class _ShoppingState extends State<Shopping> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: GetX<MyController>(builder: (controller) {
-        return ListView.builder(
-            itemCount: controller.product.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 5, right: 5, top: 5),
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 5.0,
-                    mainAxisSpacing: 5.0,
-                  ),
-                  itemBuilder: (BuildContext context, int index) {
-                    return Card(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(23)),
-                      child: Column(
-                        children: [
-                          Text(controller.product[index].title),
-                          Text(controller.product[index].description),
-                          Image.network(controller.product[index].image)
-                        ],
-                      ),
-                    );
-                  },
+        return Padding(
+          padding: const EdgeInsets.only(left: 5, right: 5, top: 5),
+          child: GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 5.0,
+              mainAxisSpacing: 5.0,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(23)),
+                child: Column(
+                  children: [
+                    Text(controller.product[index].title),
+                    Text(controller.product[index].description),
+                    Image.network(controller.product[index].image)
+                  ],
                 ),
               );
-            });
+            },
+          ),
+        );
       }),
     );
   }
